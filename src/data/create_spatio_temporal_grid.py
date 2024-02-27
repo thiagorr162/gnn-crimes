351bde8a78f20ad229e474b70f0d572a4f571b3f
Marvin <marvinmarvex@gmail.com>
2024-02-27 17:27:08 -0300

    (WIP) Refactor code to create ST grid and add a map visualization via folium

--- src/data/create_spatio_temporal_grid.py
+++ src/data/create_spatio_temporal_grid.py
@@ -1,69 +1,54 @@
+import folium
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
 # ---------------------------------------------------------------------- TRATAMENTO DO DATASET ------------------------------------------------------------
-
+print("Reading dataset")
 data = pd.read_csv("data/SF-Full_2003-2018.csv")
-
+print("Done!")
 ## retirando posições que não fazem sentido
 
+print("Processing dataset")
 data = data[data["Y"] != 90].reset_index(drop=True)
 
 ## Retirando features inexpressivos para nós
 
 data.drop(columns=data.columns[14:], inplace=True)
 data.drop(columns=data.columns[:3], inplace=True)
+
 data.drop(columns=["X", "Y"], inplace=True)  # Retirando por falta de precisão em relação ao 'location'
 data.drop(columns=["PdDistrict", "Address"], inplace=True)  # Retirada opcional
 
-
-data_hora = []
-for i in range(len(data["Date"])):
-    dia = data["Date"][i]
-    data_hora += [dia[-4:] + "-" + dia[:2] + "-" + dia[3:5] + " " + data["Time"][i] + ":00"]
+data[["month", "day", "year"]] = data["Date"].str.split("/", expand=True)
+data["datetime"] = data["year"] + "-" + data["month"] + "-" + data["day"] + " " + data["Time"] + ":00"
 
 
 ## Transformando as informações de data e hora em timestamp, para facilitar o tratamento
 
-df_datetime = pd.DataFrame({"datetime": data_hora})
+data["datetime"] = pd.to_datetime(data["datetime"])
 
-df_datetime["datetime"] = pd.to_datetime(df_datetime["datetime"])
+data["timestamp"] = data["datetime"].astype("int64")
 
-df_datetime["timestamp"] = df_datetime["datetime"].astype("int64")
-
-df_datetime["timestamp"] = df_datetime["timestamp"].divide(
-    10**9
-)  # Isso aqui é pra escalar pq aparentemente vinha em nanosegundos
+# Para escalar pq aparentemente vem em nanosegundos
+data["timestamp"] = data["timestamp"].divide(10**9)
 # então dividindo por 10**9 fica em segundos, timestamp padrão
 
-
-data.drop(columns=["Date", "Time"], inplace=True)
-data.insert(3, "Timestamp", df_datetime["timestamp"])
+data.drop(columns=["Date", "Time", "datetime"], inplace=True)
 
 
 ## Shiftando os timestamps, subtraindo o menor de todos os outros e tranformando coordenas lon/lat em km
+data["location"] = data["location"].str.replace("POINT (", "", regex=False)
+data["location"] = data["location"].str.replace(")", "", regex=False)
+data[["lon", "lat"]] = data["location"].str.split(" ", expand=True)
+data["lon"] = data["lon"].astype(float)
+data["lat"] = data["lat"].astype(float)
+data.drop(columns=["location"], inplace=True)
 
-new_X, new_Y, new_T = [], [], []
-min_T = data["Timestamp"].min()
-
-for i in range(len(data["location"])):
-    loc = data["location"][i][7:-1].split(" ")
-
-    lon, lat = float(loc[0]), float(loc[1])
-
-    new_X += [lon]
-    new_Y += [lat]
-
-    new_T += [data["Timestamp"][i] - min_T]
+data["timestamp"] = data["timestamp"] - data["timestamp"].min()
 
 
-data.insert(3, "timestamp_shift", new_T)
-data.insert(5, "lon", new_X)
-data.insert(6, "lat", new_Y)
-data.drop(columns=["location", "Timestamp"], inplace=True)
-
-
+print("Finished")
 # ------------------------------------------------------------------------------ FUNÇÕES ---------------------------------------------------------------------------
 
 
@@ -75,28 +60,19 @@ data.drop(columns=["location", "Timestamp"], inplace=True)
 # funções auxiliares
 
 
-# closest_x = lambda x: data.iloc[(data["X_reescalado (km)"] - x).abs().argsort()[:1]]["X_reescalado (km)"].tolist()[0]
-# closest_y = lambda y: data.iloc[(data["Y_reescalado (km)"] - y).abs().argsort()[:1]]["Y_reescalado (km)"].tolist()[0]
-
-
 def vert_list(N, df):
     passo_X = (df["lon"].max() - df["lon"].min()) / N
     passo_Y = (df["lat"].max() - df["lat"].min()) / N
 
-    vertices = [[], []]
+    vertices = [[df["lon"].min()], [df["lat"].min()]]
 
-    for i in range(N + 1):
-        closest_X = data.iloc[(data["lon"] - (passo_X * i)).abs().argsort()[:1]]["lon"].tolist()[0]
-        closest_Y = data.iloc[(data["lat"] - (passo_Y * i)).abs().argsort()[:1]]["lat"].tolist()[0]
-
-        vertices[0] += [closest_X]
-        vertices[1] += [closest_Y]
-
+    for i in range(1, N + 1):
+        vertices[0] += [vertices[0][0] + i * passo_X]
+        vertices[1] += [vertices[1][0] + i * passo_Y]
     return vertices
 
 
 teste = vert_list(10, data)
-print(teste)
 
 ## Segunda função: Recebe o nosso dataframe, 4 coordenadas, um x inicial e um final, um y inicial e um final
 ## e um intervalo de tempo em segundos. Nos devolve um dataframe reduzido contendo os dados da região
@@ -105,16 +81,41 @@ print(teste)
 
 def df_reduzido(df, x_initial, x_final, y_initial, y_final, t_initial, t_final):
     new_df = df.loc[
-        (df["Timestamp_shift"] >= t_initial)
-        & (df["Timestamp_shift"] <= t_final)
+        (df["timestamp"] >= t_initial)
+        & (df["timestamp"] <= t_final)
         & (df["lon"] >= x_initial)
         & (df["lat"] >= y_initial)
         & (df["lon"] <= x_final)
         & (df["lat"] <= y_final)
     ]
-
+    breakpoint()
     return new_df
 
 
-new_df = df_reduzido(data, teste[0][0], teste[0][1], teste[1][8], teste[1][9], 0, data["Timestamp_shift"].max())
-breakpoint()
+new_df = df_reduzido(data, teste[0][0], teste[0][1], teste[1][8], teste[1][9], 0, data["timestamp"].max())
+
+
+### ----------------------------------------------------------------- MAPA -----------------------------------------------------------------------
+
+
+# create a map
+this_map = folium.Map(prefer_canvas=True)
+
+
+def plotDot(point):
+    """input: series that contains a numeric named latitude and a numeric named longitude
+    this function creates a CircleMarker and adds it to your this_map"""
+    folium.CircleMarker(location=[point["lat"], point["lon"]], radius=2, weight=5).add_to(this_map)
+
+
+# use df.apply(,axis=1) to "iterate" through every row in your dataframe
+new_df.apply(plotDot, axis=1)
+
+
+# Set the zoom to the maximum possible
+this_map.fit_bounds(this_map.get_bounds())
+
+# Save the map to an HTML file
+# this_map.save('html_map_output/simple_dot_plot.html')
+
+this_map
