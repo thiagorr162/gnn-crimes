78afb1cf40bc74a0634fb278ef6c8cab54a95bd9
thiagorr162 <thiagorr162@gmail.com>
2024-02-28 19:42:26 -0300

    Add script to download crime data

---
+++ src/utils/download_data.py
@@ -0,0 +1,11 @@
+import pathlib
+
+import gdown
+
+raw_output_data_folder = pathlib.Path("data/raw")
+raw_output_data_folder.mkdir(parents=True, exist_ok=True)
+
+dataset_ids = {"full_sf.csv": "1OwJ8tU9WTLIS6POPFOt9j028zBTXOxvW", "small_sf.npz": "1jX9AQz5orzy5ShV8uo2pZwOh85VnJQIe"}
+for name, dataset_id in dataset_ids.items():
+    output_file = str(raw_output_data_folder) + "/" + name
+    gdown.download(id=dataset_id, output=output_file)
