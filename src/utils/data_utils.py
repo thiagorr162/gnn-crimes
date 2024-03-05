823144e4a6e5c7c208df66b15526c2d171ab1282
thiagorr162 <thiagorr162@gmail.com>
2024-03-05 19:57:23 -0300

    Make possible to use another column for time gap when restricting dataset

--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -1,3 +1,8 @@
+import pathlib
+
+import pandas as pd
+
+
 def create_grid(N, df):
     passo_X = (df["longitude"].max() - df["longitude"].min()) / N
     passo_Y = (df["latitude"].max() - df["latitude"].min()) / N
@@ -11,13 +16,44 @@ def create_grid(N, df):
     return vertices
 
 
-def restrict_dataset(df, x_initial, x_final, y_initial, y_final, t_initial, t_final):
+def restrict_dataset(
+    df,
+    x_initial,
+    x_final,
+    y_initial,
+    y_final,
+    t_initial,
+    t_final,
+    time_column_name="timestamp",
+):
+    assert time_column_name in df.columns
+
     new_df = df.loc[
-        (df["timestamp"] >= t_initial)
-        & (df["timestamp"] <= t_final)
+        (df[time_column_name] >= t_initial)
+        & (df[time_column_name] <= t_final)
         & (df["longitude"] >= x_initial)
         & (df["latitude"] >= y_initial)
         & (df["longitude"] <= x_final)
         & (df["latitude"] <= y_final)
     ]
     return new_df
+
+
+if __name__ == "__main__":
+    processed_dataset_path = pathlib.Path("data/processed/processed_full_sf.csv")
+    df = pd.read_csv(processed_dataset_path)
+
+    n_grid = 20
+
+    spatio_temporal_grid = create_grid(n_grid, df)
+
+    restricted_df = restrict_dataset(
+        df,
+        spatio_temporal_grid[0][10],
+        spatio_temporal_grid[0][12],
+        spatio_temporal_grid[1][8],
+        spatio_temporal_grid[1][10],
+        df["timestamp"].max() // 2,
+        df["timestamp"].max(),
+    )
+    breakpoint()
